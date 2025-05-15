a7c6e33ee4920adabef93c7c65d60d89fb92fffa
98001/yash <yashchauhan.gaya@gmail.com>
2025-05-15 22:22:57 +0530

    a

---
+++ user-service/src/main/java/com/company/SafarSaathi/user_service/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.company.SafarSaathi.user_service.exceptions;
+
+public class BadRequestException extends RuntimeException{
+
+  public BadRequestException(String message){
+      super(message);
+  }
+}

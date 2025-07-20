c0e5f96a551c79ed4cc240cd431079da3ca6ac60
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-20 14:55:28 +0530

    Added the enums in the service

---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/enums/GenderPreference.java
@@ -0,0 +1,6 @@
+package com.company.SafarSaathi.companion_service.enums;
+
+public enum GenderPreference {
+
+    MALE, FEMALE, ANY
+}
---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/enums/TravelType.java
@@ -0,0 +1,6 @@
+package com.company.SafarSaathi.companion_service.enums;
+
+public enum TravelType {
+
+    SOLO, FRIENDS, COUPLE, FAMILY, ANY
+}

69d75bfc8610931730b3877d7a91a08254603b4e
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-20 14:53:31 +0530

    Added the Companion entity

---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/entity/Companion.java
@@ -0,0 +1,35 @@
+package com.company.SafarSaathi.companion_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "companions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Companion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long tripId;
+    private Long userId;
+
+
+    private String status;
+    private String message;
+
+
+    @ElementCollection
+    @CollectionTable(name = "companion_matches", joinColumns = @JoinColumn(name = "companion_id"))
+    @Column(name = "matched_user_id")
+    private Set<Long> matchedUserIds = new HashSet<>();
+}

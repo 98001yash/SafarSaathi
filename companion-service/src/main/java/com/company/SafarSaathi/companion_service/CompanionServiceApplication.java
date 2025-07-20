dae49908b74c8d097ceb667aa55d485e92f57325
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-20 14:06:51 +0530

    added the companion service in the SafarSaathi application

---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/CompanionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.SafarSaathi.companion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionServiceApplication.class, args);
+	}
+
+}
---
+++ companion-service/src/test/java/com/company/SafarSaathi/companion_service/CompanionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.SafarSaathi.companion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

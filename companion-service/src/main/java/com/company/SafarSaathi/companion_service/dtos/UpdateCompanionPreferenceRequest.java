0be6b10cd6c117761b8363a4e4aeb4b16467bdb8
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-21 07:45:44 +0530

    added the two more dtos createCompnionRequest and updateCompanionPreferenceRequest

---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/dtos/UpdateCompanionPreferenceRequest.java
@@ -0,0 +1,26 @@
+package com.company.SafarSaathi.companion_service.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateCompanionPreferenceRequest {
+
+    private Integer preferredAgeMin;
+
+    private Integer preferredAgeNax;
+
+    private String preferredGender;
+
+    private Boolean allowSmokers;
+
+    private Boolean allowDrinkers;
+
+    private String travelType;
+}

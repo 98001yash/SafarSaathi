b3717aa45078f65ee84e7c0a4343436e03b0b0ed
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-26 10:27:40 +0530

    updated the matchingService

---
+++ matching-service/src/main/java/com/company/SafarSaathi/matching_service/service/MatchingService.java
@@ -0,0 +1,32 @@
+package com.company.SafarSaathi.matching_service.service;
+
+
+import com.company.SafarSaathi.matching_service.client.MatchingClient;
+import com.company.SafarSaathi.matching_service.dtos.CompanionProfile;
+import com.company.SafarSaathi.matching_service.dtos.MatchRequest;
+import com.company.SafarSaathi.matching_service.dtos.MatchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MatchingService {
+
+
+    private final MatchingClient matchingClient;
+
+    public List<CompanionProfile> findTopMatches(CompanionProfile user, List<CompanionProfile> candidates) {
+        MatchRequest request = new MatchRequest();
+        request.setUserProfile(user);
+        request.setCandidates(candidates);
+
+        MatchResponse response = matchingClient.getTopMatches(request);
+
+        return response.getMatches().stream()
+                .map(result -> candidates.get(result.getIndex()))
+                .collect(Collectors.toList());
+    }
+}

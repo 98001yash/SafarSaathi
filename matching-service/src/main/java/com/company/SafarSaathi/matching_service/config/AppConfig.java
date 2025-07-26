9fdd45991f717f9cde07894193e8f86f42d70aee
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-26 10:10:35 +0530

    added the matchingClient for calling the python fastAPI

--- matching-service/src/main/java/com/company/SafarSaathi/matching_service/config/AppConfig.java
+++ matching-service/src/main/java/com/company/SafarSaathi/matching_service/config/AppConfig.java
@@ -4,6 +4,7 @@ package com.company.SafarSaathi.matching_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +12,11 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }

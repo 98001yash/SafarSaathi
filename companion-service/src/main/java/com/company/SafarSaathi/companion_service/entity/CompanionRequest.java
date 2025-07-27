f22a8072f4cd5351b4dc702a20b527414d899795
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-27 09:17:58 +0530

    added rhe companion_requests entity in the comopanion service

---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/entity/CompanionRequest.java
@@ -0,0 +1,34 @@
+package com.company.SafarSaathi.companion_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name  ="companion_requests")
+public class CompanionRequest {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long senderId;
+    private Long receiverId;
+
+    private Long tripId;
+
+    @Enumerated(EnumType.STRING)
+    private RequestStatus status;
+
+    private LocalDateTime timeStamp;
+}

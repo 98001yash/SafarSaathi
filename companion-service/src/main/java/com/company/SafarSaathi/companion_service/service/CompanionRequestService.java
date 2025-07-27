23a1a285f618e8080d84a0d28ec176a42c2102d1
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-27 09:36:40 +0530

    added rhe acceptRequest

--- companion-service/src/main/java/com/company/SafarSaathi/companion_service/service/CompanionRequestService.java
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/service/CompanionRequestService.java
@@ -5,6 +5,7 @@ import com.company.SafarSaathi.companion_service.dtos.CompanionRequestDto;
 import com.company.SafarSaathi.companion_service.dtos.CompanionRequestResponseDto;
 import com.company.SafarSaathi.companion_service.entity.CompanionRequest;
 import com.company.SafarSaathi.companion_service.enums.RequestStatus;
+import com.company.SafarSaathi.companion_service.exceptions.ResourceNotFoundException;
 import com.company.SafarSaathi.companion_service.repository.CompanionRequestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,5 +35,20 @@ public class CompanionRequestService {
         log.info("New companion Request sent from {} to {}",dto.getSenderId(), dto.getReceiverId());
 
         return modelMapper.map(saved, CompanionRequestResponseDto.class);
+    }
+
+
+    public CompanionRequestResponseDto acceptRequest(Long requestId){
+        CompanionRequest request = companionRequestRepository.findById(requestId)
+                .orElseThrow(()->{
+                    log.error("Request ID {} not found with ACCEPT",requestId);
+                    return new ResourceNotFoundException("Request not found");
+                });
+
+        request.setStatus(RequestStatus.ACCEPTED);
+        companionRequestRepository.save(request);
+        log.info("Request ID {} accepted",requestId);
+
+        return modelMapper.map(request, CompanionRequestResponseDto.class);
     }
 }

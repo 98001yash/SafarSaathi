cf2a5374a68aef3b020909a55f598a19d11657b4
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-07-31 07:19:57 +0530

    updated the savePreference in the companionPreferenceService

--- companion-service/src/main/java/com/company/SafarSaathi/companion_service/service/CompanionPreferenceService.java
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/service/CompanionPreferenceService.java
@@ -1,5 +1,6 @@
 package com.company.SafarSaathi.companion_service.service;
 
+import com.company.SafarSaathi.companion_service.auth.UserContextHolder;
 import com.company.SafarSaathi.companion_service.entity.CompanionPreference;
 import com.company.SafarSaathi.companion_service.repository.CompanionPreferenceRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,11 @@ public class CompanionPreferenceService {
     private final CompanionPreferenceRepository preferenceRepository;
 
     public void savePreference(CompanionPreference preference) {
+        Long userId = UserContextHolder.getCurrentUserId(); // <-- Get userId securely
+        preference.setUserId(userId);                       // <-- Inject into entity
         preferenceRepository.save(preference);
     }
+
 
     public Optional<CompanionPreference> getPreferenceByUserId(Long userId) {
         return preferenceRepository.findByUserId(userId);

a906e30affe13ccf42c83a963bd61cd1a730325a
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-08-01 16:27:00 +0530

    userDto added in the user-service

--- user-service/src/main/java/com/company/SafarSaathi/user_service/dtos/UserDto.java
+++ user-service/src/main/java/com/company/SafarSaathi/user_service/dtos/UserDto.java
@@ -1,18 +1,35 @@
 package com.company.SafarSaathi.user_service.dtos;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDto {
 
-    private Long id;
-    private String name;
-    private String email;
+   private String id;
+   private String fullName;
+   private String email;
+   private String phoneNumber;
 
-    private Integer age;
-    private String gender;
-    private Boolean smoker;
-    private Boolean drinker;
+   private String gender;
+   private int age;
+
+   private String bio;
+   private String country;
+   private String city;
+
+   private boolean smoker;
+   private boolean drinker;
+
+   private String lifeStyle;
+   private String travelStyle;
+
+   private String profileImageUrl;
 
 }

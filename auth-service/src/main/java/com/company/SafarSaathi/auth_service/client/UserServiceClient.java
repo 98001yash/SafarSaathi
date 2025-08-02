241d2da1227aea72c79b18f7e1798c50c4045502
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-08-02 10:15:04 +0530

    working good

--- auth-service/src/main/java/com/company/SafarSaathi/auth_service/client/UserServiceClient.java
+++ auth-service/src/main/java/com/company/SafarSaathi/auth_service/client/UserServiceClient.java
@@ -2,7 +2,7 @@ package com.company.SafarSaathi.auth_service.client;
 
 
 
-import com.company.SafarSaathi.auth_service.dtos.UserDto;
+
 import com.company.SafarSaathi.auth_service.dtos.UserProfileCreateRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;

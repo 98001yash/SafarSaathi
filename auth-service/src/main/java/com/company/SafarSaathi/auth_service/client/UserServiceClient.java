90d8e099625e2a48c752e901f035532500ca5f9c
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-08-02 09:56:21 +0530

    updated the userServiceClient

--- auth-service/src/main/java/com/company/SafarSaathi/auth_service/client/UserServiceClient.java
+++ auth-service/src/main/java/com/company/SafarSaathi/auth_service/client/UserServiceClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
 
-    @PostMapping("/profile")
-    UserDto createUser(@RequestBody UserProfileCreateRequest request);
+    @PostMapping("/users/profile")
+    UserProfileCreateRequest createUser(@RequestBody UserProfileCreateRequest request);
 }
 

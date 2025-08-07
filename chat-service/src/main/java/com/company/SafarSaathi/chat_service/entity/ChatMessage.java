92b233ec171a38727422209380e20ef131145679
Yash chauhan <yashchauhan.gaya@gmail.com>
2025-08-07 09:44:07 +0530

    added the chatMessage entity

---
+++ chat-service/src/main/java/com/company/SafarSaathi/chat_service/entity/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.company.SafarSaathi.chat_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_messages")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long senderId;
+    private Long receiverId;
+
+    private String content;
+    private LocalDateTime timeStamp;
+    private boolean seen;
+}
